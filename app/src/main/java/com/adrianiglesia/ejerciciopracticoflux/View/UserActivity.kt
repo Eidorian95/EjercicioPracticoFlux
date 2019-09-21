e0b42a9548d94313c1f112d48db407df51316f73
Eidorian95 <adriglesia95@gmail.com>
2019-09-21 14:37:03 -0300

    COMENTARIO DE PRUEBA

--- app/src/main/java/com/adrianiglesia/ejerciciopracticoflux/View/UserActivity.kt
+++ app/src/main/java/com/adrianiglesia/ejerciciopracticoflux/View/UserActivity.kt
@@ -9,5 +9,8 @@ class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+
+
+        //COMENTARIO DE PRUEBA
     }
 }
